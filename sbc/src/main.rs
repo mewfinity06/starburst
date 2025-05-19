af7be432c3168e4d29bfb5e817cd310fe4e39b57
Ashton Sickler <ashton.sickler@icloud.com>
2025-05-18 21:06:04 -0500

    cleanup

--- sbc/src/main.rs
+++ sbc/src/main.rs
@@ -73,38 +73,3 @@ fn read_entire_file_buffered(path: &str) -> String {
         .expect("Failed to read file");
     contents
 }
-
-// use lexer::Lexer;
-// use lexer::tokens::TokenKind::*;
-
-// fn main() {
-//     let content = r#"--functions
-// const add | func [a: Int, b: Int] -> Int : {
-//     a + b
-// }
-
-// const sub | func [a, b: Int] -> Int : a - b;
-
-// val v1 |: add[a: 2, b: 8]
-// val v2 |: sub[3, 6]
-//         "#;
-//     let lexer = Lexer::new(content);
-
-//     println!("{}", content);
-
-//     let mut counter: usize = 0;
-//     for token in lexer {
-//         if counter % 3 == 0 {
-//             println!();
-//         }
-//         match token.kind {
-//             DocComment | Comment => continue,
-//             Identifier | BuiltinType | StringLiteral | CharLiteral | IntLiteral => {
-//                 print!("'{}' ", &content[token.span.start..token.span.end])
-//             }
-//             _ => print!("{:?} ", token.kind),
-//         }
-//         counter += 1;
-//     }
-//     println!();
-// }
